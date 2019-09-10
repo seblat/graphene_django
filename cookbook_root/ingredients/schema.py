0d151b89a77f099abd15e9966eae16595641a152
Sebastian <sebastian@serioese.gmbh>
2019-09-10 12:07:17 +0200

    feat(schema): allow retrieving single objects

--- cookbook_root/ingredients/schema.py
+++ cookbook_root/ingredients/schema.py
@@ -15,7 +15,14 @@ class IngredientOT(DjangoObjectType):
 
 
 class Query(object):
+    category = graphene.Field(CategoryOT,
+                              id=graphene.ID(),
+                              name=graphene.String())
     all_categories = graphene.List(CategoryOT)
+
+    ingredient = graphene.Field(IngredientOT,
+                                id=graphene.ID(),
+                                name=graphene.String())
     all_ingredients = graphene.List(IngredientOT)
 
     def resolve_all_categories(self, info, **kwargs):
@@ -23,3 +30,21 @@ class Query(object):
 
     def resolve_all_ingredients(self, info, **kwargs):
         return Ingredient.objects.select_related('category').all()
+
+    def resolve_category(self, info, id=None, name=None, **kwargs):
+        if id:
+            return Category.objects.get(id=id)
+
+        if name:
+            return Category.objects.get(name=name)
+
+        return None
+
+    def resolve_ingredient(self, info, id=None, name=None, **kwargs):
+        if id:
+            return Ingredient.objects.get(id=id)
+
+        if name:
+            return Ingredient.objects.get(name=name)
+
+        return None
